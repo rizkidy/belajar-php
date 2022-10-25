b09cd1e1519f691598eae702e25272c1a0d515f8
Rizki Dwi Yulianto <rizkidy99@gmail.com>
2022-10-25 21:39:39 +0700

    belajar php D1

--- belajar1.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$umurKakak = 22;
-$umurAdek = 7;
-
-echo 'Belajar PHP Dasar 1';
-echo 'Hasil =  $umurKakak - $umurAdek';
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+$namaSaya = "Rizki Dwi Yulianto";
+$umurSaya = 22;
+$umurAdek = 7;
+$selisihUmur = $umurSaya - $umurAdek;
+
+echo "Belajar PHP Dasar 1 <br>";
+echo "Nama saya <strong>$namaSaya</strong>, saya berusia <strong>$umurSaya</strong> tahun. <br>";
+echo "Selisih umur saya dengan adek adalah = $selisihUmur tahun.";
+
+ ?>
