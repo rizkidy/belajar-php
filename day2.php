623153ea9d6e62a36899b63a0157527ed013d908
Rizki Dwi Yulianto <rizkidy99@gmail.com>
2022-10-27 13:03:23 +0700

    perulangan dan percabangan php

--- day2.php
+++ day2.php
@@ -81,17 +81,49 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">
-    <label>Nama</label>
-    <input type="text" name="nama">
-    
+    <h1>Pamer Mobil Klean</h1>
+    <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
+        <label>Merek Mobil</label>
+        <input type="text" name="mobil">
+        <label>Total</label>
+        <input type="text" name="total">
+        <input type="submit" name="submit">
     </form>
+
+    <?php
+    if (!empty($_POST['submit'])) {
+
+        switch ($_POST['mobil']) {
+            case "Avanza";
+                $pesan = "Kamu punya mobil merek " . $_POST['mobil'];
+                break;
+            case "Lamborghini";
+                $pesan = "Kamu punya mobil merek " . $_POST['mobil'];
+                break;
+            case "Panther";
+                $pesan = "Kamu punya mobil merek " . $_POST['mobil'];
+                break;
+            default:
+                $pesan = "Skip merek mobilmu ga terkenal";
+        }
+
+        for ($i = 0; $i < $_POST['total']; $i++) {
+            $n = $i + 1;
+            echo $pesan . " total : $n mobil." . "<br>";
+        }
+    } else {
+        echo "Kamu belum punya mobil ya?";
+    }
+    ?>
 </body>
+
 </html>
